1ccdb11b3121075194713211c2615d20437401ef
MandarPande <mandarpande29@gmail.com>
2023-03-13 16:15:54 +0530

    Added join and leave functionality

--- beatbot/main.py
+++ beatbot/main.py
@@ -2,7 +2,7 @@ import os
 import random
 
 import discord
-from discord import Member, Message
+from discord import Member, Message, VoiceProtocol
 from discord.ext import commands
 from discord.ext.commands import Context
 from dotenv import load_dotenv
@@ -41,6 +41,30 @@ async def on_member_join(member: Member):
 async def nine_nine(ctx: Context):
     response = random.choice(brooklyn_99_quotes)
     await ctx.send(response)
+
+
+@bot.command(name='join', help='Tells the bot to join the voice channel')
+async def join(ctx: Context):
+    # todo : check if bot is already connected to the same vc
+    voice_client = ctx.message.guild.voice_client
+    if not ctx.message.author.voice:
+        await ctx.send(f'{ctx.message.author.voice} is not connected to a voice channel')
+        return
+    elif voice_client.is_connected():
+        await ctx.send(f'{ctx.message.author.voice} is already connect to voice channel')
+        return
+    else:
+        channel = ctx.message.author.voice.channel
+    await channel.connect()
+
+
+@bot.command(name='leave', help='Tells the bot to leave the voice channel')
+async def leave(ctx: Context):
+    voice_client = ctx.message.guild.voice_client
+    if voice_client.is_connected():
+        await voice_client.disconnect()
+    else:
+        await ctx.send("The bot is not connected to a voice channel.")
 
 
 bot.run(TOKEN)
